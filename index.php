a52fd75281edbfe94aff78a69f46732951a9569c
jefferson felizardo <jefferson@iMac-de-jefferson.local>
2019-11-12 15:16:39 -0300

    Começando pagina de login do facebook

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Login facebook</title>
+</head>
+
+<body>
+    <header>
+        <div class="center">
+            <div class="logo">
+                <h2>facebook</h2>
+            </div>
+            <!-- logo -->
+            <div class="form-login">
+
+            </div>
+            <!-- form login -->
+        </div>
+        <!-- center -->
+    </header>
+
+</body>
+
+</html>
