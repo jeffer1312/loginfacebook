bc9216342032901fdd6e8fb8e136e9564e92906c
jefferson felizardo <jefferson@iMac-de-jefferson.local>
2019-11-13 20:12:02 -0300

    Section content em andamento, usando display flex

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="./fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="./css/style.css">
     <title>Login facebook</title>
 </head>
 
 <body>
     <header>
-        <div class="center">
+        <div class="container">
             <div class="logo">
                 <h2>facebook</h2>
             </div>
@@ -30,13 +32,155 @@
                     <input type="submit" name="acao" value="Entrar">
                 </div>
             </form>
-
-
             <!-- form-login -->
             <div class="clear"></div>
+
         </div>
-        <!-- center -->
+        <!-- container -->
     </header>
+    <section class="content">
+        <div class="container flex ">
+            <div class="w50">
+                <div class="wraper">
+                    <div class="recent">
+                        <h2>Login recentes</h2>
+                        <p>clique na sua foto ou adicione uma conta.</p>
+                    </div>
+                    <div class="flex row">
+                        <div class="img">
+                            <img src="./images/user.jpg">
+                            <p>jefferson</p>
+                        </div>
+                        <div class="add">
+                            <h2><i class="fas fa-plus-circle"></i></h2>
+                            <p>adicionar conta</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w50">
+                <div class="wrap">
+                    <div class="title">
+                        <h2>Criar uma nova conta</h2>
+                        <p>É rápido e fácil</p>
+                    </div>
+
+                    <form class="cadastro ">
+                        <input type="text" name="Nome" placeholder="Nome">
+                        <input type="text" name="sobrenome" placeholder="Sobrenome">
+                        <input type="email" name="email" placeholder="Celular ou email">
+                        <input type="password" name="senha" placeholder="Nova senha">
+                    </form>
+                    <div class="born">
+                        <p>Data de nascimento</p>
+                        <span class="dia-aniversario">
+                                <select aria-label="dia">
+                                    <option value="0">dia</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    <option value="21">21</option>
+                                    <option value="22">22</option>
+                                    <option value="23">23</option>
+                                    <option value="24">24</option>
+                                    <option value="25">25</option>
+                                    <option value="26">26</option>
+                                    <option value="27">27</option>
+                                    <option value="28">28</option>
+                                    <option value="29">29</option>
+                                    <option value="30">30</option>
+                                    <option value="31">31</option>
+                                </select>
+                            </span>
+                        <span class="mes-aniversario">
+                                    <select aria-label="mes">
+                                      <option value="0">Mes</option>
+                                      <option value="1">Jan</option>
+                                      <option value="2">Fev</option>
+                                      <option value="3">Mar</option>
+                                      <option value="4">Mai</option>
+                                      <option value="5">Abr</option>
+                                      <option value="6">Jun</option>
+                                      <option value="7">Jul</option>
+                                      <option value="8">Ago</option>
+                                      <option value="9">Set</option>
+                                      <option value="10">Out</option>
+                                      <option value="11">Nov</option>
+                                      <option value="12">Dez</option>
+                                    </select>
+                            </span>
+                        <span class="ano-aniversario">
+                                    <select aria-label="ano">
+                                      <option value="0">Ano</option>
+                                      <option value="1">2019</option>
+                                      <option value="2">2018</option>
+                                      <option value="3">2017</option>
+                                      <option value="4">2016</option>
+                                      <option value="5">2015</option>
+                                      <option value="6">2014</option>
+                                      <option value="7">2013</option>
+                                      <option value="8">2012</option>
+                                      <option value="9">2011</option>
+                                      <option value="10">2010</option>
+                                      <option value="11">2009</option>
+                                      <option value="12">2008</option>
+                                      <option value="13">2007</option>
+                                      <option value="14">2006</option>
+                                      <option value="15">2005</option>
+                                      <option value="16">2004</option>
+                                      <option value="17">2003</option>
+                                      <option value="18">2002</option>
+                                      <option value="19">2001</option>
+                                      <option value="20">2000</option>
+                                      <option value="21">1999</option>
+                                      <option value="22">1998</option>
+                                      <option value="23">1997</option>
+                                      <option value="24">1996</option>
+                                      <option value="25">1995</option>
+                                      <option value="26">1994</option>
+                                      <option value="27">1993</option>
+                                      <option value="28">1992</option>
+                                      <option value="29">1991</option>
+                                      <option value="30">1990</option>
+                                      <option value="31">1989</option>
+                                      <option value="32">1988</option>
+                                      <option value="33">1987</option>
+                                      <option value="34">1986</option>
+                                      <option value="35">1985</option>
+                                      <option value="36">1984</option>
+                                      <option value="37">1983</option>
+                                      <option value="38">1982</option>
+                                      <option value="39">1981</option>
+                                      <option value="40">1980</option>                             
+                                    
+                                     
+                                    </select>
+                            </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- container -->
+
+    </section>
+    <!-- content -->
 
 </body>
 
